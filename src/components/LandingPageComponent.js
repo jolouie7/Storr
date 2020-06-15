367a9d3b240f2e5620ea717ac1aa97e95133367e
Your_Name <joseph161216@gmail.com>
2020-06-14 20:56:47 -0700

    update navbar component

--- src/components/LandingPageComponent.js
+++ src/components/LandingPageComponent.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
 
 function LandingPageComponent() {
   return (
     <div>
-      LandingPageComponent
+      <Container>
+        <Jumbotron>
+          <h1>Hello, world!</h1>
+          <p>
+            This is a simple hero unit, a simple jumbotron-style component for
+            calling extra attention to featured content or information.
+          </p>
+          <p>
+            <Button variant="primary">Learn more</Button>
+          </p>
+        </Jumbotron>
+      </Container>
     </div>
-  )
+  );
 }
 
 export default LandingPageComponent
--- src/components/NavBarComponent.js
+++ src/components/NavBarComponent.js
@@ -1,28 +1,43 @@
 import React from 'react'
+import { useAuth0 } from "../react-auth0-spa";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Button from "react-bootstrap/Button";
 import { Link, useHistory } from "react-router-dom";
 
-function NavBarComponent() {
+const NavBarComponent = () => {
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0;
   const history = useHistory();
 
   return (
     <div>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Link to="/">
-          <Navbar.Brand>
-            Storr
-          </Navbar.Brand>
+          <Navbar.Brand>Storr</Navbar.Brand>
         </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ml-auto">
             <Nav.Link>
-              <Button variant="dark" onClick={() => history.push("/SignIn")}>Sign In</Button>
+              {/* <Button variant="dark" onClick={() => history.push("/SignIn")}>Sign In</Button> */}
+              <div>
+                {!isAuthenticated && (
+                  <Button variant="dark" onClick={() => loginWithRedirect({})}>
+                    Log in
+                  </Button>
+                )}
+
+                {isAuthenticated && (
+                  <Button variant="dark" onClick={() => logout()}>
+                    Log out
+                  </Button>
+                )}
+              </div>
             </Nav.Link>
             <Nav.Link>
-              <Button variant="primary" onClick={() => history.push("/SignUp")}>Sign Up</Button>
+              <Button variant="primary" onClick={() => history.push("/SignUp")}>
+                Sign Up
+              </Button>
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
