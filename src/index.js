82e1c115cabc717c27e98d62b73dd6fa46dd907a
Your_Name <joseph161216@gmail.com>
2020-07-09 18:57:10 -0700

    setup store in index.js file

--- src/index.js
+++ src/index.js
@@ -7,6 +7,15 @@ import * as serviceWorker from "./serviceWorker";
 import { Auth0Provider } from "./react-auth0-spa";
 import config from "./auth_config.json";
 import history from "./utils/history";
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers/RootReducer";
+
+// This allows you to use Redux dev tools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middleware = [thunk];
+const store = createStore(reducer, composeEnhancers(applyMiddleware(...middleware)));
 
 // A function that routes the user to the right place
 // after login
@@ -18,6 +27,7 @@ const onRedirectCallback = (appState) => {
   );
 };
 
+// ! If unable to store user info then try wrapping the entire Auth0Provider in Provider
 ReactDOM.render(
   <Auth0Provider
     domain={config.domain}
@@ -25,7 +35,9 @@ ReactDOM.render(
     redirect_uri={window.location.origin}
     onRedirectCallback={onRedirectCallback}
   >
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </Auth0Provider>,
   document.getElementById("root")
 );
